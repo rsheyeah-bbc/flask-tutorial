37ec638c84d048c9eb93bf9b1d8011fefb29f0f0
rsheyeah-bbc <arshiyahussain.bbc@gmail.com>
2019-10-02 11:38:37 +0100

    Added /api

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import jsonify
+import requests
+import json
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -7,3 +10,12 @@ app.config["DEBUG"] = True
 @app.route("/")
 def hello():
     return "Hello World! Does Flask auto-update? Do you need to reload the server? It updated"
+
+
+@app.route("/api")
+def helloapi():
+    data = {'Colorado':'Rockies',
+            'Boston': 'RedSox'}
+    json_data = jsonify(data)
+
+    return json_data
